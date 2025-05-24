6c76df01c58f5a2620710cb50f3b30f95555bffc
Andrew Chen <ache337@aucklanduni.ac.nz>
2025-05-24 23:58:00 +1200

    figuring out how to structure the code and started coding for task 1

--- src/main/java/nz/ac/auckland/se281/MapEngine.java
+++ src/main/java/nz/ac/auckland/se281/MapEngine.java
@@ -1,24 +1,47 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
 import java.util.List;
+
+// HashSet or LinkedHashSet (Set<__> var = new HashSet<>() OR new LinkedHashSet<>();)
+// LinkedList with the Queue (Queue<__> var = new LinkedList<>();)
+// LinkedList/ArrayList with the List DONE
+// Throw and catch custom exception
+// At least two new classes.
 
 /** This class is the main entry point. */
 public class MapEngine {
 
+  private List<String> countryStats;
+  private List<String> adjacencies;
+  private List<String> countryNames = new ArrayList<>();
+  private boolean isInvalidCountry = true;
+
   public MapEngine() {
-    // add other code here if you wan
-    loadMap(); // keep this mehtod invocation
+    loadMap();
   }
 
   /** invoked one time only when constracting the MapEngine class. */
   private void loadMap() {
 
-    List<String> countries = Utils.readCountries();
-    List<String> adjacencies = Utils.readAdjacencies();
+    this.countryStats = Utils.readCountries();
+    this.adjacencies = Utils.readAdjacencies();
+    for (String country : this.countryStats) {
+      countryNames.add(country.split(",")[0].trim());
+    }
   }
 
   /** this method is invoked when the user run the command info-country. */
-  public void showInfoCountry() {}
+  public void showInfoCountry() {
+    MapEngine map = new MapEngine();
+    while (isInvalidCountry) {
+      MessageCli.INSERT_COUNTRY.printMessage();
+      String inputCountry = Utils.scanner.nextLine().trim();
+      if (countryNames.contains(Utils.capitalizeFirstLetterOfEachWord(inputCountry))) {
+        isInvalidCountry = false;
+      }
+    }
+  }
 
   /** this method is invoked when the user run the command route. */
   public void showRoute() {}
